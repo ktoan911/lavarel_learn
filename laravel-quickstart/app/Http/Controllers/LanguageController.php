5a489851354092d7aa895b4270ecd93a9062bf2d
Nguyen Khanh Toan <khanhtoan.forwork@gmail.com>
2025-08-01 00:11:06 +0700

    chapter 8+9

---
+++ laravel-quickstart/app/Http/Controllers/LanguageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class LanguageController extends Controller
+{
+    public function changeLanguage(Request $request, $language ) {
+        session(['lang' => $language]);
+
+        return redirect()->back();
+    }
+}
+
+
