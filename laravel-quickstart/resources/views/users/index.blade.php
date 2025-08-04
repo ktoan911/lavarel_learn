87ed485cd5dd1db9ecf2669b8cee96ed51346544
Nguyen Khanh Toan <khanhtoan.forwork@gmail.com>
2025-08-04 14:19:41 +0700

    chapter12

--- laravel-quickstart/resources/views/users/index.blade.php
+++ laravel-quickstart/resources/views/users/index.blade.php
@@ -15,12 +15,17 @@
             <x-primary-button class="mt-4">
                 {{ __('Create') }}
             </x-primary-button>
+            {{-- N+1 redundancy --}}
+            {{-- Lazy coding khi laravel chỉ lấy ra những thứ cần dẫn đến việc nếu không báo trước cho nó cần các trường gì nó sẽ load 
+            thiếu và mất công load lại câu truy vấn khi gặp trường không có --}}
+            {{-- -> sửa trong controller để lấy ra các trường cần thiết --}}
             <table class="table">
                 <thead>
                     <tr>
                         <th class="text-gray-900 dark:text-gray-100" scope="col">ID</th>
                         <th class="text-gray-900 dark:text-gray-100" scope="col">Name</th>
                         <th class="text-gray-900 dark:text-gray-100" scope="col">Username</th>
+                        <th class="text-gray-900 dark:text-gray-100" scope="col">Tasks</th>
                         <th class="text-gray-900 dark:text-gray-100" scope="col">Action</th>
                     </tr>
                 </thead>
@@ -31,9 +36,16 @@
                             <td class="text-gray-900 dark:text-gray-100">{{ $user->fullname }}</td>
                             <td class="text-gray-900 dark:text-gray-100">{{ $user->username }}</td>
                             <td class="text-gray-900 dark:text-gray-100">
-                                <x-primary-button class="mt-4">
-                                    {{ __('Edit') }}
-                                </x-primary-button>
+                                @foreach ($user->tasks as $task)
+                                    {{ $task->name }} 
+                                @endforeach
+                            </td>
+                            <td class="text-gray-900 dark:text-gray-100">
+                                <a href="/user/{{ $user->id }}/edit">
+                                    <x-primary-button class="mt-4">
+                                        {{ __('Edit') }}
+                                    </x-primary-button>
+                                </a>
                                 <x-primary-button class="mt-4">
                                     {{ __('Delete') }}
                                 </x-primary-button>
